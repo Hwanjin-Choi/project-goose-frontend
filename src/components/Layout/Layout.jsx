d7ae667d8a0a4701ed84500e3817b5b277883de7
Hwanjin-Choi <68840464+Hwanjin-Choi@users.noreply.github.com>
2025-05-16 10:53:36 +0900

    Fix layout issue

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,17 +1,14 @@
 // Layout.js
 import React from "react";
-import styled, { ThemeProvider } from "styled-components"; // ThemeProvider 추가
+import styled, { ThemeProvider } from "styled-components";
 import Header, { HEADER_HEIGHT } from "../Header/Header";
 
-// 테마 객체 정의: 공통 스타일 값(예: maxWidth)을 관리합니다.
 const theme = {
-  maxWidth: "1200px", // 스크린샷과 유사한 콘텐츠 최대 너비 (예시, 필요에 따라 조정)
-  // 다른 공통 스타일 값들을 여기에 추가할 수 있습니다.
-  // mobileBreakpoint: '768px',
+  maxWidth: "1200px",
 };
 
 const AppWrapper = styled.div`
-  min-height: 100vh;
+  /* min-height: 100vh; */
   padding-top: ${HEADER_HEIGHT};
   margin: 0 auto;
   @media (min-width: 768px) {
@@ -27,12 +24,14 @@ const LayoutContainer = styled.div`
   position: relative;
   box-sizing: border-box;
   overflow-y: scroll;
-  min-height: 100vh;
+  min-height: calc(100vh - ${HEADER_HEIGHT});
   height: 100%;
 `;
 
 const MainContent = styled.main`
   padding: 20px; /* 콘텐츠 내부의 패딩 */
+  min-height: calc(100vh - ${HEADER_HEIGHT});
+  height: 100%;
 `;
 
 const Layout = ({ children }) => {
--- src/pages/SignUpPage/Signup_Page.jsx
+++ src/pages/SignUpPage/Signup_Page.jsx
@@ -2,27 +2,18 @@ import React from "react";
 import SignUp_Form from "../../components/Signup/Signup_Form";
 import styled, { createGlobalStyle } from "styled-components";
 
-
-const GlobalStyle = createGlobalStyle`
-  html, body {
-    overflow: hidden;
-  }
-`;
-
 const PageWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  min-height: 100vh;
+  height: 100%;
   padding: 0px;
   box-sizing: border-box;
-  background-color: #f4f4f4;
 `;
 
 const SignUP_Page = () => {
   return (
     <>
-      <GlobalStyle />
       <PageWrapper>
         <SignUp_Form />
       </PageWrapper>
