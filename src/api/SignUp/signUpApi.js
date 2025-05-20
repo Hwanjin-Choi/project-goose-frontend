3fc125bcf96d70701c911af4d9481d9b398c9a7a
Lookies09 <edcvfr40679051@gmail.com>
2025-05-20 15:32:01 +0900

    add resetRegistration state controll
    회원가입 창 redux로 상태 컨트롤

--- src/api/SignUp/signUpApi.js
+++ src/api/SignUp/signUpApi.js
@@ -9,12 +9,26 @@ export const SignUp = async ({ username, password, nickname, admin }) => {
       nickname,
       admin,
     });
-    return response.data;
+    if (
+      response.data &&
+      (response.data.status === "SUCCESS" || response.status === 200)
+    ) {
+      return response.data;
+    } else {
+      throw new Error(
+        response.data.message ||
+          "회원가입 요청에 실패했습니다. 잠시 후 다시 시도해주세요."
+      );
+    }
   } catch (error) {
     if (error.response && error.response.data && error.response.data.error) {
       const errorMessage = error.response.data.message;
       console.error("회원가입 실패:", errorMessage);
       throw new Error(errorMessage);
+    } else if (error.request) {
+      throw new Error(
+        "서버로부터 응답을 받지 못했습니다. 네트워크 상태를 확인해주세요."
+      );
     } else {
       console.error("회원가입 요청 실패:", error);
       throw new Error(
